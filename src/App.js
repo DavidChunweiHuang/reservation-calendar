27590b57508244a8ecd82579a90f814cdebe9f68
David Chun-Wei Huang <57789567+DavidChunweiHuang@users.noreply.github.com>
2026-04-03 12:26:13 +0800

    Update App.js
    remove useRef due to not using it

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
